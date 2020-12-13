6f09250c39bbab3f216abe64358c68ecf28c489b
devops19u <50083858+devops19u@users.noreply.github.com>
2020-12-13 21:52:16 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('<html><body style="background-color:powderblue;"><h1 style="color:red;text-align: center;">Welcome to AWS-Devops World!!</h1></body></html> \n');
+  res.send('<html><body style="background-color:#00FF00;"><h1 style="color:red;text-align: center;">Welcome to AWS-Devops World!!</h1></body></html> \n');
 });
 
 app.listen(PORT, HOST);
